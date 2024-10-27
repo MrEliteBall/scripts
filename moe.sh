5451ba821c6b6c191dd25eb96053167868d03897
Shoiya Akari <akariondev@gmail.com>
2024-10-27 13:25:09 -0300

    moe.sh: inline kernel support

--- moe.sh
+++ moe.sh
@@ -7,7 +7,7 @@ SECONDS=0
 CLANG_VERSION="clang-17.0.0"
 TC_DIR="$HOME/tc/$CLANG_VERSION"
 PATH=$HOME/tc/$CLANG_VERSION/bin:$PATH
-export modpath=AnyKernel3/modules/vendor/lib/modules
+#export modpath=AnyKernel3/modules/vendor/lib/modules
 export ARCH=arm64
 export KBUILD_BUILD_USER=Moe
 export KBUILD_BUILD_HOST=Nyan
@@ -69,41 +69,31 @@ make ${ARGS} O=out -j$(nproc)
 echo "  ERROR : image binary not found in any of the specified locations , fix compile!" && \
 exit 1
 
-make O=out ${ARGS} -j$(nproc) INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
 
 if [ -d "$AK3_DIR" ]; then
 	cp -r $AK3_DIR AnyKernel3
 	git -C AnyKernel3 checkout bangkk &> /dev/null
-elif ! git clone -q https://github.com/MoeKernel/AnyKernel3 -b bangkk; then
+elif ! git clone -q https://github.com/MoeKernel/AnyKernel3 -b inline; then
 	echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
 	exit 1
 fi
 
-mkdir -p ${modpath}
+#mkdir -p ${modpath}
 kver=$(make kernelversion)
 kmod=$(echo ${kver} | awk -F'.' '{print $3}')
-mkdir -p AnyKernel3/modules/vendor/lib/modules 
+#mkdir -p AnyKernel3/modules/vendor/lib/modules
 kver=$(make kernelversion)
 kmod=$(echo ${kver} | awk -F'.' '{print $3}')
+
 cp out/.config AnyKernel3/config
 cp out/arch/arm64/boot/Image AnyKernel3/Image
 cp out/arch/arm64/boot/dtb.img AnyKernel3/dtb
 cp out/arch/arm64/boot/dtbo.img AnyKernel3/dtbo.img
-# cp build.sta/${DEVICE}_modules.blocklist ${modpath}/modules.blocklist
-cp $(find out/modules/lib/modules/5.4* -name '*.ko') ${modpath}/
-cp out/modules/lib/modules/5.4*/modules.{alias,dep,softdep} ${modpath}/
-cp out/modules/lib/modules/5.4*/modules.order ${modpath}/modules.load
-sed -i 's/\(kernel\/[^: ]*\/\)\([^: ]*\.ko\)/\/vendor\/lib\/modules\/\2/g' ${modpath}/modules.dep
-sed -i 's/.*\///; s/\.ko$//' ${modpath}/modules.load
-source build.sta/${DEVICE}_mdconf
-for useles_modules in "${modules_to_nuke[@]}"; do
-  grep -vE "$useles_modules" ${modpath}/modules.load > /tmp/templd && mv /tmp/templd ${modpath}/modules.load
-done
+
 cd AnyKernel3
 zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
 cd ..
 echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
 echo "Zip: $ZIPNAME"
-#curl -F "file=@$ZIPNAME" https://temp.sh/upload
 rm -rf AnyKernel3
