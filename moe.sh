607afaed1bf2c8fe853af66d837569a997ac9372
Shoiya Akari <akariondev@gmail.com>
2024-10-30 14:39:52 -0300

    moe.sh: update src

--- moe.sh
+++ moe.sh
@@ -7,6 +7,7 @@ SECONDS=0
 CLANG_VERSION="clang-17.0.0"
 TC_DIR="$HOME/tc/$CLANG_VERSION"
 PATH=$HOME/tc/$CLANG_VERSION/bin:$PATH
+export modpath=AnyKernel3/modules/vendor/lib/modules
 export ARCH=arm64
 export KBUILD_BUILD_USER=Moe
 export KBUILD_BUILD_HOST=Nyan
@@ -61,6 +62,7 @@ LLVM_NM='${LLVM_DIR}/llvm-nm'
 LLVM=1
 '
 
+make ${ARGS} O=out $DEFCONFIG moto.config
 make ${ARGS} O=out -j$(nproc)
 
 if [ ! -e "out/arch/arm64/boot/Image" ]; then
@@ -68,13 +70,14 @@ if [ ! -e "out/arch/arm64/boot/Image" ]; then
     exit 1
 fi
 
+make O=out ${ARGS} -j$(nproc) INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install
 echo -e "\nKernel compiled successfully for $DEFCONFIG! Zipping up...\n"
 
 if [ -d "$AK3_DIR" ]; then
     cp -r $AK3_DIR AnyKernel3
     git -C AnyKernel3 checkout bangkk &> /dev/null
 else
-    git clone -q https://github.com/MoeKernel/AnyKernel3 -b inline
+    git clone -q https://github.com/MoeKernel/AnyKernel3 -b nfc
 fi
 
 cp out/.config AnyKernel3/config
@@ -83,6 +86,30 @@ cp out/arch/arm64/boot/dtb.img AnyKernel3/dtb
 cp out/arch/arm64/boot/dtbo.img AnyKernel3/dtbo.img
 
 ZIPNAME="${ZIPNAME_PREFIX}-${VARIANT}.zip"
+
+mkdir -p ${modpath}
+kver=$(make kernelversion)
+kmod=$(echo ${kver} | awk -F'.' '{print $3}')
+mkdir -p AnyKernel3/modules/vendor/lib/modules 
+kver=$(make kernelversion)
+kmod=$(echo ${kver} | awk -F'.' '{print $3}')
+
+cp out/.config AnyKernel3/config
+cp out/arch/arm64/boot/Image AnyKernel3/Image
+cp out/arch/arm64/boot/dtb.img AnyKernel3/dtb
+cp out/arch/arm64/boot/dtbo.img AnyKernel3/dtbo.img
+cp $(find out/modules/lib/modules/5.4* -name '*.ko') ${modpath}/
+cp out/modules/lib/modules/5.4*/modules.{alias,dep,softdep} ${modpath}/
+cp out/modules/lib/modules/5.4*/modules.order ${modpath}/modules.load
+sed -i 's/\(kernel\/[^: ]*\/\)\([^: ]*\.ko\)/\/vendor\/lib\/modules\/\2/g' ${modpath}/modules.dep
+sed -i 's/.*\///; s/\.ko$//' ${modpath}/modules.load
+
+source build.sta/${DEVICE}_mdconf
+
+for useles_modules in "${modules_to_nuke[@]}"; do
+  grep -vE "$useles_modules" ${modpath}/modules.load > /tmp/templd && mv /tmp/templd ${modpath}/modules.load
+done
+
 cd AnyKernel3
 zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
 cd ..
@@ -90,5 +117,3 @@ cd ..
 echo -e "\nCompleted compilation for $DEFCONFIG (variant $VARIANT) in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!"
 echo "Zip: $ZIPNAME"
 rm -rf AnyKernel3
-
-echo -e "\nAll compilations finished!"
